51b65b3592fb5c5315be66993feb268188f03fc5
Fran Delgado <thinway@gmail.com>
2016-07-02 18:16:42 +0200

    Initial Commit

---
+++ Baccus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baccus
+//
+//  Created by Fran Delgado on 2/7/16.
+//  Copyright © 2016 Fran Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baccus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Baccus
+//
+//  Created by Fran Delgado on 2/7/16.
+//  Copyright © 2016 Fran Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
